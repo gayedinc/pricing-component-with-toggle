1debfff8dbd3b353e50ac8f7b3cc036623f66e32
[Gaye Dinç] <gayedinc190@gmail.com>
2024-11-05 14:12:56 +0300

    fazlalıklar kaldırıldı

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -6,12 +6,10 @@ function handleMonthlyPayment() {
   if (aylikOdeme.checked) {
     for (const num of priceNumbers) {
       num.innerHTML = Number((num.innerHTML - 0.09) / 10).toFixed(2);
-      updatedPrices.push(num.innerHTML);
     }
   } else {
     for (const num of priceNumbers) {
       num.innerHTML = Number((num.innerHTML * 10) + 0.09).toFixed(2);
-      updatedPrices.push(num.innerHTML);
     }
   }
 }
