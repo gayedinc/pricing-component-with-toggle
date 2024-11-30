4e52a3043f855c8a5c8e64298de603a856880563
[Gaye Dinç] <gayedinc190@gmail.com>
2024-12-01 01:13:41 +0300

    yorum satırları eklendi

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,14 +1,15 @@
 const priceNumbers = document.querySelectorAll('.priceNum');
 
+// aylikOdeme id'li switch'e tıklandığında handleMonthlyPayment çalışacak
 aylikOdeme.addEventListener('click', handleMonthlyPayment);
 
 function handleMonthlyPayment() {
-  if (aylikOdeme.checked) {
-    for (const num of priceNumbers) {
+  if (aylikOdeme.checked) { // aylık ödeme gösterilecekse
+    for (const num of priceNumbers) { // tüm fiyatların üzerinde dönüyor
       num.innerHTML = Number((num.innerHTML - 0.09) / 10).toFixed(2);
     }
-  } else {
-    for (const num of priceNumbers) {
+  } else { // yıllık ödeme gösteriliyorsa
+    for (const num of priceNumbers) { // tüm fiyatların üzerinde dönüyor
       num.innerHTML = Number((num.innerHTML * 10) + 0.09).toFixed(2);
     }
   }
